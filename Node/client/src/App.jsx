e71e50d137ed5db5ae9c83d8d3896f41e5a66189
yuzhongyao <yuzhong_yao@hotmail.com>
2022-12-25 22:12:44 -0500

    add react routes

--- Node/client/src/App.jsx
+++ Node/client/src/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Players from "./routes/Players";
+import Teams from "./routes/Teams";
+import PlayerDetails from "./routes/PlayerDetails";
+import TeamDetails from "./routes/TeamDetails";
+
+
+
 
 const App = () => {
     return <div>App
-        <Router>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/" component={Home}/>
-
-        </Router>
+        <BrowserRouter>
+            <Routes> 
+                      
+                    <Route exact path="/" element={<App/>}/>
+                    <Route exact path="players" element={<Players/>}/>
+                    <Route exact path="/teams" element={<Teams/>}/>
+                    <Route exact path="/players/:playerid" element={<PlayerDetails/>}/>
+                    <Route exact path="/Teams/:teamid" element={<TeamDetails/>}/>
+                 
+             </Routes>
+        </BrowserRouter>
     </div>;
 };
 
