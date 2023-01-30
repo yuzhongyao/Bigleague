d5588e82299feb8c63e68cae3958086846f698a1
NathanDubale <114881899+NathanDubale@users.noreply.github.com>
2023-01-30 03:40:05 +0000

    Update Navbar.jsx

--- Node/client/src/components/Navbar.jsx
+++ Node/client/src/components/Navbar.jsx
@@ -1,26 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './style.css';
+import { IoMdBasketball } from 'react-icons/io';
+
+
+
 
 const Navbar = () => {
 
     return (
-        <div>
         <nav className="navbar">
-            <div className="container-fluid">
-                <div>
-                    <Link to="/Games" className='me-5 display-5 text-decoration-none'>Games</Link>
-                    <Link to="/Teams" className='me-5 display-5 text-decoration-none'>Teams</Link>
-                    <Link to="/Players" className='me-5 display-5 text-decoration-none'>Players</Link>
-                    <Link to="/Info" className='me-5 display-5 text-decoration-none'>Info</Link>
+      <div className="navbar__container">
+      
 
-                </div>
-            </div>
-        </nav>
+      <ul className="navbar__head">
+        
+          <li className="navbar__head">
+          
+          <a href="/" class="navbar__head">BIG LEAGUE</a>
+          <IoMdBasketball className='header-icon' />
+          <img id="main__img" src="https://yt3.googleusercontent.com/Yn8kDo0rIfDOpBO9g-eleFQ2WwxZqgdtDbvxPUgrVSgNPOH94you8K5RJnP1WBXyAYYnXZ98Eg=s88-c-k-c0x00ffffff-no-rj" />
+          </li>
 
+      </ul>
+      
+
+      
+
+
+      
+        
+      
+      
+       
+     
+      
+       <div className="navbar__toggle" id="mobile-menu">
+          <span className="bar"></span> <span className="bar"></span>
+          <span className="bar"></span>
+          
         </div>
+        <ul className="navbar__menu">
+        
+          <li className="navbar__item">
+            <a href="Games" class="navbar__links">Games</a>
+          </li>
+          <li className="navbar__item">
+            <a href="/teams" class="navbar__links">Teams</a>
+          </li>
+          <li className="navbar__item">
+            <a href="/Players" className="navbar__links">Players</a>
+          </li>
+          <li className="navbar__item">
+          <a href="Info" className="navbar__links">Info</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+     );
 
-    );
+     
 
 };
+
+
 
 export default Navbar;
