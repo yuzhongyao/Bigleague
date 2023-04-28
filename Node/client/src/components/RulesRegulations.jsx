9be3afd08b285eab210296ad01b472c46c1db431
yuzhongyao <yuzhong_yao@hotmail.com>
2023-04-28 04:44:07 -0400

    Update RulesRegulations.jsx

--- Node/client/src/components/RulesRegulations.jsx
+++ Node/client/src/components/RulesRegulations.jsx
@@ -14,10 +14,15 @@ const RulesRegulations = () => {
                     Officials are instructed to automically eject players who attempt to throw punches regardless if the punch connected. All ejections come with a 1 game suspension. Other situations will be left to the discretion of officials.
                     We reserve the right to review each case to suspend further or remove entirely the involved persons with <b>NO REFUNDS</b>.</p>
                 <h2 className="Underline">2. Rules</h2>
-                    <p>Officials are following FIBA rules with slight modifications</p>
+                    <p>Officials are following FIBA rules with slight modifications<br></br>
+                    Players are not allowed to touch the ball around the net after it first touches like in FIBA or else a goaltending will be called.
+                <h2 className="Underline">2. Fouls</h2>    
+                    <p>Players are allowed 5 player fouls. On the 5th player foul, that player must leave the game and cannot return.<br></br>
+                    Each team is allowed 8 team fouls for each half. On the 8th foul and each foul thereafter will result in double bonus. The opposing player that got fouled will be awarded 2 free throws.<br></br>
+                    Technical fouls on a player will be counted towards that players allowed 5 fouls and team fouls.</p>
                 <h2 className="Underline">3. Game Length</h2>    
-                    <p>Games are two 20 minute halves.<br></br>
-                    Running time. The time only stops in the last two minutes of the last half if the score is within 7 points.
+                    <p>Games are two 20 minute halves running time.<br>
+                    </br> The time only stops in the last two minutes of the last half if the score is within 7 points (6 points and under).
                     </p>
                 <h2 className="Underline">4.Timeouts</h2>
                     <p>Each time has 1 30 second timeout for the entire game.</p>
@@ -25,9 +30,9 @@ const RulesRegulations = () => {
                     <p>There is no shot clock.</p>
                 <h2 className="Underline">6.Overtime</h2>
                     <p>First overtime is 2 minutes with possession first going with the possession arrow. <br>
-                    </br>Second overtime is 1 minute. Third overtime is sudden death.</p>
+                    </br>Second overtime is 1 minute. Third overtime is sudden death where the first team to score will win.</p>
                 <h2 className="Underline">7.Late/Forfeits</h2>
-                    <p>Time will start at scheduled time. Each team needs at least 5 dressed players to play. Each minute a team is ineligible to play will result in 2 points awarded to the opposing team. If 10 minutes have passed then the team will forfeit the game.</p>
+                    <p>Time will start at scheduled time. Each team needs at least 5 dressed players to play. Each minute a team is ineligible to play will result in 2 points awarded to the opposing team. If 10 minutes have passed then the team will forfeit the game and lose 20-0.</p>
                 <h2 className="Underline">8.Standings</h2>
                     <p>Team standings are based on wins. If multiple teams have the same wins, point differential will be used.</p>
         </div>
