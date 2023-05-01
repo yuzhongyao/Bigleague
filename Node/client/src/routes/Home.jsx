fa6bbdbb8cf9d1ad9dd211e04cc9342f37f31829
NathanDubale <114881899+NathanDubale@users.noreply.github.com>
2023-05-01 04:15:42 +0000

    Update Home.jsx

--- Node/client/src/routes/Home.jsx
+++ Node/client/src/routes/Home.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Footer from '../components/inAllPages/Footer';
 import Header from "../components/inAllPages/Header";
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 import Schedule from '../components/Schedule';
 
 const Home = () => {
     return (
     <div>
         <div className='row'>
-            <div className='col'><Header/></div>
+            
             <div className='col'><Navbar/></div>
         </div>
+        <Hero/>
         <Schedule/>
         <Footer/>
     </div>
