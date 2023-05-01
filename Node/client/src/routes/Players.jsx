07ff12219d3ecb4e29cc15f979013b6cb5511ae0
NathanDubale <114881899+NathanDubale@users.noreply.github.com>
2023-05-01 04:26:13 +0000

    Update Players.jsx

--- Node/client/src/routes/Players.jsx
+++ Node/client/src/routes/Players.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Footer from '../components/inAllPages/Footer';
-import Header from "../components/inAllPages/Header";
 import Navbar from '../components/Navbar';
 import PlayersList from '../components/players/PlayersList';
 
 const Players = () => {
     return (<div>
                  <div className='row'>
-                    <div className='col'><Header/></div>
+                    
                     <div className='col'><Navbar/></div>
                 </div>
                 <PlayersList/>
