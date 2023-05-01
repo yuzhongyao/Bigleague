a79715d0c4e0e36f296f7bad5c58e19106c2a5bc
NathanDubale <114881899+NathanDubale@users.noreply.github.com>
2023-05-01 04:21:16 +0000

    Update Teams.jsx

--- Node/client/src/routes/Teams.jsx
+++ Node/client/src/routes/Teams.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from "../components/inAllPages/Header";
 import Navbar from '../components/Navbar';
 import TeamsList from "../components/teams/TeamsList";
 import Footer from "../components/inAllPages/Footer";
@@ -7,7 +6,7 @@ import Footer from "../components/inAllPages/Footer";
 const Teams = () => {
     return (<div>
         <div className='row'>
-            <div className='col'><Header/></div>
+         
             <div className='col'><Navbar/></div>
         </div>
         <TeamsList/>
